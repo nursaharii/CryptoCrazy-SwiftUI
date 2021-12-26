3d181d8e8beca5ab12cd93cba957cd2af274c7a4
nursah <nursahari@hotmail.com>
2021-12-26 16:48:55 +0300

    Initial Commit

---
+++ CryptoCrazy-SwiftUI/CryptoCrazy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazy_SwiftUIApp.swift
+//  CryptoCrazy-SwiftUI
+//
+//  Created by Nurşah on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
